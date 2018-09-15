a12bc3f83d8e69db25b38644816b6f4bdc83f07f
MasatoMakino <unagiinu.g@gmail.com>
2018-09-15 20:16:53 +0900

    不要なコンソール処理を削除。

--- node-newer-files.js
+++ node-newer-files.js
@@ -40,12 +40,8 @@ const isNeedsUpdate = (filePath, srcDir, targetDir) => {
   //ハッシュ比較　変更があったら上書き対象
   const hashSrc = md5File.sync(path.resolve(srcDir, filePath));
   const hashTarget = md5File.sync(path.resolve(targetDir, filePath));
-  console.debug(filePath, hashSrc, hashTarget, hashSrc !== hashTarget);
-  if (hashSrc !== hashTarget) {
-    return true;
-  }
 
-  return false;
+  return hashSrc !== hashTarget;
 };
 
 /**
