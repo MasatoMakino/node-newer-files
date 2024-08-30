4cff41de98356356a7f31f0bd12f7da52b4e3100
Masato Makino <makino.masato.g@gmail.com>
2024-08-30 22:04:10 +0900

    migrate : type "module" (#75)
    * change : ESM
    
    * migtare : esm
    
    * add : vitest.config

--- spec/node-newer-files.spec.js
+++ spec/node-newer-files.spec.js
@@ -1,11 +1,12 @@
 "use strict";
-const fs = require("fs");
+import fs from "fs";
+import { vi, describe, test, expect } from "vitest";
 
-const spyLog = jest.spyOn(console, "log").mockImplementation((x) => x);
-const spyWarn = jest.spyOn(console, "warn").mockImplementation((x) => x);
-const spyError = jest.spyOn(console, "error").mockImplementation((x) => x);
+const spyLog = vi.spyOn(console, "log").mockImplementation((x) => x);
+const spyWarn = vi.spyOn(console, "warn").mockImplementation((x) => x);
+const spyError = vi.spyOn(console, "error").mockImplementation((x) => x);
 
-const newer = require("../node-newer-files");
+import { getFiles, sync } from "../node-newer-files";
 
 describe("更新ファイルリストの取得", () => {
   const maybeList = [
@@ -43,36 +44,28 @@ describe("更新ファイルリストの取得", () => {
   */
 
   test("ファイル拡張子無指定の場合は空の配列を返す", () => {
-    const list = newer.getFiles(undefined, "spec/sampleSrc", "spec/sampleDist");
+    const list = getFiles(undefined, "spec/sampleSrc", "spec/sampleDist");
     expect(list).toEqual([]);
   });
 
   test("ソースディレクトリ未指定の場合はエラーとして処理を中断", () => {
-    const list = newer.getFiles(
-      ["js", "html", "css"],
-      undefined,
-      "spec/sampleDist",
-    );
+    const list = getFiles(["js", "html", "css"], undefined, "spec/sampleDist");
     expect(list).toEqual([]);
   });
 
   test("ターゲットディレクトリ未指定の場合は空として扱い、ソースディレクトリのファイルを全て返す", () => {
-    const list = newer.getFiles(
-      ["js", "html", "css"],
-      "spec/sampleSrc",
-      undefined,
-    );
+    const list = getFiles(["js", "html", "css"], "spec/sampleSrc", undefined);
     expect([...list].sort()).toEqual([...srcList].sort());
   });
 });
 
 describe("ファイルの同期", () => {
-  const unlink = jest
+  const unlink = vi
     .spyOn(fs, "unlink")
     .mockImplementation((string, Function) => undefined);
 
   test("ファイルの同期と削除を行う", () => {
-    const deleted = newer.sync(
+    const deleted = sync(
       ["js", "html", "css"],
       "spec/sampleSrc",
       "spec/sampleDist",
@@ -88,7 +81,7 @@ describe("ファイルの同期", () => {
   });
 
   test("srcディレクトリが存在しない場合は停止", () => {
-    const deleted = newer.sync(
+    const deleted = sync(
       ["js", "html", "css"],
       "not/exist/dir",
       "spec/sampleDist",
@@ -101,25 +94,21 @@ describe("ファイルの同期", () => {
   });
 
   test("srcディレクトリがnullの場合も存在しないとして処理停止", () => {
-    const deleted = newer.sync(["js", "html", "css"], null, "spec/sampleDist");
+    const deleted = sync(["js", "html", "css"], null, "spec/sampleDist");
     expect(deleted).toBe(false);
     unlink.mockReset();
     unlink.mockRestore();
   });
 
   test("srcディレクトリがundefinedの場合も存在しないとして処理停止", () => {
-    const deleted = newer.sync(
-      ["js", "html", "css"],
-      undefined,
-      "spec/sampleDist",
-    );
+    const deleted = sync(["js", "html", "css"], undefined, "spec/sampleDist");
     expect(deleted).toBe(false);
     unlink.mockReset();
     unlink.mockRestore();
   });
 
   test("targetディレクトリが存在しない場合は停止", () => {
-    const deleted = newer.sync(
+    const deleted = sync(
       ["js", "html", "css"],
       "spec/sampleSrc",
       "not/exist/dir",
@@ -130,25 +119,21 @@ describe("ファイルの同期", () => {
   });
 
   test("targetディレクトリがundefinedの場合は処理停止", () => {
-    const deleted = newer.sync(
-      ["js", "html", "css"],
-      "spec/sampleSrc",
-      undefined,
-    );
+    const deleted = sync(["js", "html", "css"], "spec/sampleSrc", undefined);
     expect(deleted).toBe(false);
     unlink.mockReset();
     unlink.mockRestore();
   });
 
   test("targetディレクトリがルート'./'の場合は処理停止", () => {
-    const deleted = newer.sync(["js", "html", "css"], "spec/sampleSrc", "./");
+    const deleted = sync(["js", "html", "css"], "spec/sampleSrc", "./");
     expect(deleted).toBe(false);
     unlink.mockReset();
     unlink.mockRestore();
   });
 
   test("targetディレクトリが'/'の場合は処理停止", () => {
-    const deleted = newer.sync(["js", "html", "css"], "spec/sampleSrc", "/");
+    const deleted = sync(["js", "html", "css"], "spec/sampleSrc", "/");
     expect(deleted).toBe(false);
     unlink.mockReset();
     unlink.mockRestore();
